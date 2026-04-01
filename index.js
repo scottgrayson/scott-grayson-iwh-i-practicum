5b14613f51011f65db94554b412c1241d7dbbeae
Scott Grayson <dscottgrayson@gmail.com>
2026-04-01 08:09:21 -0400

    Add homepage route skeleton

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ const headers = {
     'Content-Type': 'application/json'
 };
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
+// ROUTE 1 - Homepage: display custom object records in a table
+app.get('/', async (req, res) => {
+    res.render('homepage', { title: 'Houseplants | Integrating With HubSpot I Practicum', data: [] });
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
